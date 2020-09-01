2496032d6dbbf2556e950ea58e0410ce19f68aa3
zhenik <nikita.zhevnitskiy@gmail.com>
2020-09-01 15:29:03 +0200

    add examples upload, update doc, test

--- outputs.tf
+++ outputs.tf
@@ -6,9 +6,11 @@ output "minio_service_name" {
 output "minio_access_key" {
   description = "Minio access key"
   value       = data.template_file.nomad-job-minio.vars.access_key
+  sensitive   = true
 }
 
 output "minio_secret_key" {
   description = "Minio secret key"
   value       = data.template_file.nomad-job-minio.vars.secret_key
+  sensitive   = true
 }

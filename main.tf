dd8b6fc23b0b13a12e06c1b3b9ee1b8f886c787e
m77773 <karima.cherradi@skatteetaten.no>
2020-11-18 13:07:28 +0100

    Oppdatert main.tf with master

--- main.tf
+++ main.tf
@@ -48,9 +48,7 @@ data "template_file" "nomad_job_minio" {
     envs                  = local.minio_env_vars
     use_host_volume       = var.use_host_volume
     use_canary            = var.use_canary
-    upstreams       = jsonencode(var.minio_upstreams)
-    cpu_proxy                 = var.resource_proxy.cpu
-    memory_proxy              = var.resource_proxy.memory
+    upstreams             = jsonencode(var.minio_upstreams)
 
   }
 }
@@ -73,8 +71,7 @@ data "template_file" "nomad_job_mc" {
     vault_kv_access_key   = var.vault_secret.vault_kv_access_key
     vault_kv_secret_key   = var.vault_secret.vault_kv_secret_key
     envs                  = local.mc_env_vars
-
-    command = local.command
+    command               = local.command
   }
 }
 
